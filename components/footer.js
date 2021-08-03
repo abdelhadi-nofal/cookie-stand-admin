04a4027c43e8e7110fbefc1f12c27956dc2bf95a
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-08-03 23:39:52 +0300

    fin

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+export default function CookieStandFooter(props) {
+    return (
+        <footer className= 'text-l py-2 font-thin px-4 bg-green-500 text-black'>
+        {props.branches} Locations Worldwide
+        </footer>
+    )
+    
+}
