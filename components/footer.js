90a6fd18fedfedf723a85b6d922777d7a30da6f8
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-08-08 12:44:10 +0300

    lab-39

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function CookieStandFooter(props) {
     return (
         <footer className= 'text-l py-2 font-thin px-4 bg-green-500 text-black'>
-        {props.branches} Locations Worldwide
+        {props.locations} Locations Worldwide
         </footer>
     )
     
